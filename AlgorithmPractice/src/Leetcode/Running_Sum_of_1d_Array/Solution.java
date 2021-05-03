cd4eebd635d4fabbba8754b4de7a6ead1e4ff224
sangki930 <sangki931@gmail.com>
2021-05-03 22:53:12 +0900

    2021-05-03 commit

---
+++ AlgorithmPractice/src/Leetcode/Running_Sum_of_1d_Array/Solution.java
@@ -0,0 +1,13 @@
+package Leetcode.Running_Sum_of_1d_Array;
+
+class Solution {
+    public int[] runningSum(int[] nums) {
+        int answer[]=new int[nums.length];
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum+=nums[i];
+            answer[i]=sum;
+        }
+        return answer;
+    }
+}
